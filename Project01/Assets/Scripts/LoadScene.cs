f656fc80be38095a39414bd29090e0f88d50e45c
Luke A <alvarengalucas55@gmail.com>
2016-05-13 21:42:18 -0300

    Basic Unity project
    A basic Unity project with a main screen tha contains a clickable
    button.

---
+++ Project01/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+	public void ButtonLoadScene(int index)
+	{
+		Application.LoadLevel(index);
+	}
+	
+	public void ButtonLoadScene(string levelName)
+	{
+		Application.LoadLevel(levelName);
+	}
+}
